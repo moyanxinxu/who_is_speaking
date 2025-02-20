ba4087b1b2104150a01fdccd35f301e9fcdda39c
JianxiangTian <wwxy.mail@gmail.com>
2025-02-20 20:54:41 +0800

    feat: 更新分词器部分的代码

--- src/modules/tokenizer.py
+++ src/modules/tokenizer.py
@@ -1,62 +1,10 @@
-import json
-import os
-from typing import Union
-
-import numpy as np
-import torch
-
-from src.utils import hp
+from transformers import WhisperTokenizer
 
 
-class WhisperTokenizerForDiarization:
-    def __init__(self):
-        self.vocab = self.load_vocab(hp.vocab_path)
-        self.bos_token = self.vocab["<|startofdiarization|>"]
-        self.lang_token = self.vocab["<|zh|>"]
-        self.eos_token = self.vocab["<|endofdiarization|>"]
-        self.prefix_token = [self.bos_token, self.lang_token]
-        self.suffix_token = [self.eos_token]
-        self.id2token = {v: k for k, v in self.vocab.items()}
+class WhisperTokenizerForDiarization(WhisperTokenizer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._add_speaker_token()
 
-    def load_vocab(self, vocab_path: str) -> dict[str, int]:
-        assert os.path.isfile(vocab_path), "vocab file not found"
-
-        with open(vocab_path, "r") as json_obj:
-            vocab = json.load(json_obj)
-
-        return vocab
-
-    def add_special_token(self, input_ids: Union[list[int], np.ndarray]):
-        if isinstance(input_ids, np.ndarray):
-            input_ids = input_ids.tolist()
-            return np.array(self.prefix_token + input_ids + self.suffix_token)
-        if isinstance(input_ids, list):
-            return self.prefix_token + input_ids + self.suffix_token
-
-    def decode(self, input_ids: Union[list[int], np.ndarray]) -> str:
-        tokens = [self.id2token[i] for i in input_ids]
-        return tokens
-
-    def pad(
-        self, text: list[torch.Tensor], dynamic_padding: bool = True
-    ) -> list[torch.Tensor]:
-        max_length = None
-        if dynamic_padding:
-            max_length = max([t.size(0) for t in text])
-        else:
-            max_length = hp.max_length
-
-        for i, t in enumerate(text):
-            text[i] = torch.cat(
-                [
-                    t,
-                    torch.tensor([self.vocab["<|endofdiarization|>"]] * max_length),
-                ],
-                dim=0,
-            )[:max_length]
-        return text
-
-    def shift(self, text: torch.Tensor) -> torch.Tensor:
-        """在默认第一个token为起始token的情况下, 将text向右移动一位"""
-        text = torch.cat([text[:, :1], text], dim=1)[:, :-1]
-        return text
+    def _add_speaker_token(self):
+        self.add_tokens(["<speaker_changed>"])
